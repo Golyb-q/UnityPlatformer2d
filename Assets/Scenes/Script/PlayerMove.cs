a09bf8162b1cfdffee8b159d5da1af328380df2d
Golyb-q <zavy58p@gmail.com>
2023-04-28 01:27:16 +0700

    add control

--- Assets/Scenes/Script/PlayerMove.cs
+++ Assets/Scenes/Script/PlayerMove.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
@@ -9,11 +10,14 @@ public class PlayerMove : MonoBehaviour
     public float verticalSpeed;
     private float horizontalMove;
     private float verticalMove;
+    private Animator animator;
+    private bool canJump;
 
     // Start is called before the first frame update
     void Start()
     {
         rb2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -21,7 +25,42 @@ public class PlayerMove : MonoBehaviour
     {
         horizontalMove = Input.GetAxisRaw("Horizontal");
         verticalMove = Input.GetAxisRaw("Vertical");
-        if (horizontalMove != 0f || verticalMove != 0f)
-            rb2D.AddForce(new Vector2(horizontalMove * horizontalSpeed, verticalMove * verticalSpeed), ForceMode2D.Impulse);
+        if (horizontalMove != 0f)
+        {
+            rb2D.velocity = new Vector2(horizontalMove * horizontalSpeed, rb2D.velocity.y);
+            if (horizontalMove > 0)
+                transform.localScale = new Vector3(1f, 1f, 1f);
+            else
+                transform.localScale = new Vector3(-1f, 1f, 1f);
+            animator.SetBool("Walk", true);
+        }
+        else if (!animator.GetBool("Air"))
+        {
+            rb2D.velocity = new Vector2(0f, rb2D.velocity.y);
+        }
+        if (verticalMove != 0f && canJump)
+        {
+            animator.SetBool("Air", true);
+            rb2D.AddForce(new Vector2(0f, verticalMove * verticalSpeed), ForceMode2D.Impulse);
+            canJump = false;
+        }
+        if (!(verticalMove != 0f && canJump) && !(horizontalMove != 0f))
+        {
+            animator.SetBool("Walk", false);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "floor")
+        {
+            animator.SetBool("Air", false);
+            canJump = true;
+        }
+        Debug.Log("Enter: " + collision.gameObject.name + "; canJump: " + canJump);
+    }
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        Debug.Log("Exit: " + collision.gameObject.name + "; canJump: " + canJump);
     }
 }
